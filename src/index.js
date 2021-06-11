9e9543e0a3730eeff528f732576ff46ed39238f9
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-11 11:26:43 +0300

    Feat(game): add blank screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello Gamer');
+import Phaser from 'phaser';
+
+/** @type {Phaser.Types.Core.GameConfig}
+ * {width} number
+ * {height} number
+ * {type} class
+ */
+const config = {
+  width: 800,
+  height: 500,
+  type: Phaser.AUTO,
+};
+
+// eslint-disable-next-line no-new
+new Phaser.Game(config);
