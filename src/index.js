768957c4d26b7d1a9b5678e6b0a980e884fdba8f
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-11 12:09:05 +0300

    Feat(game): add game scene

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import Game from './scenes/game';
 import Phaser from 'phaser';
+import TitleScreen from './scenes/titleScreen';
 
 /** @type {Phaser.Types.Core.GameConfig}
  * {width} number
@@ -9,7 +11,18 @@ const config = {
   width: 800,
   height: 500,
   type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: true,
+    },
+  },
 };
 
 // eslint-disable-next-line no-new
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
+game.scene.add('titleScreen', TitleScreen);
+game.scene.add('game', Game);
+// game.scene.start('titleScreen');
+game.scene.start('game');
