c6b3c752034f6219eb20708bfca27de92ff33ecb
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-11 15:07:16 +0300

    Feat(game): add left paddle

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,6 +1,14 @@
 import Phaser from 'phaser';
 
 export default class Game extends Phaser.Scene {
+  paddleLeft;
+
+  paddleRight;
+
+  cursors;
+
+  leftKey;
+
   preload() {}
 
   create() {
@@ -10,12 +18,42 @@ export default class Game extends Phaser.Scene {
     ball.body.setVelocity(-200, 0);
     ball.body.setBounce(1, 1);
 
-    const paddleLeft = this.add.rectangle(30, 250, 25, 100, 0x0000ff, 1);
-    const paddleRight = this.add.rectangle(770, 250, 25, 100, 0xff0000, 1);
+    this.paddleLeft = this.add.rectangle(30, 250, 25, 100, 0x0000ff, 1);
+    this.paddleRight = this.add.rectangle(770, 250, 25, 100, 0xff0000, 1);
 
-    this.physics.add.existing(paddleLeft, true);
-    this.physics.add.existing(paddleRight, true);
-    this.physics.add.collider(paddleLeft, ball);
-    this.physics.add.collider(paddleRight, ball);
+    this.physics.add.existing(this.paddleLeft, true);
+    this.physics.add.existing(this.paddleRight, true);
+    this.physics.add.collider(this.paddleLeft, ball);
+    this.physics.add.collider(this.paddleRight, ball);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.leftKey = this.input.keyboard;
+  }
+
+  update() {
+    const { body } = this.paddleRight;
+    const leftBody = this.paddleLeft.body;
+    const W = this.leftKey.addKey('W');
+    const S = this.leftKey.addKey('S');
+
+    if (this.cursors.up.isDown) {
+      this.paddleRight.y -= 10;
+      body.updateFromGameObject();
+    }
+
+    if (this.cursors.down.isDown) {
+      this.paddleRight.y += 10;
+      body.updateFromGameObject();
+    }
+
+    if (W.isDown) {
+      this.paddleLeft.y -= 10;
+      leftBody.updateFromGameObject();
+    }
+
+    if (S.isDown) {
+      this.paddleLeft.y += 10;
+      leftBody.updateFromGameObject();
+    }
   }
 }
