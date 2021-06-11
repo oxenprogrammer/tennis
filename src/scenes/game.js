538508e24523e37737b9c6295f38e090f6491821
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-11 13:08:56 +0300

    Feat(game): add paddles

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -7,6 +7,15 @@ export default class Game extends Phaser.Scene {
     const ball = this.add.circle(400, 250, 10, 0xffffff, 1);
     this.physics.add.existing(ball);
     ball.body.setCollideWorldBounds(true, 1, 1);
-    ball.body.setVelocity(200, 200);
+    ball.body.setVelocity(-200, 0);
+    ball.body.setBounce(1, 1);
+
+    const paddleLeft = this.add.rectangle(30, 250, 25, 100, 0x0000ff, 1);
+    const paddleRight = this.add.rectangle(770, 250, 25, 100, 0xff0000, 1);
+
+    this.physics.add.existing(paddleLeft, true);
+    this.physics.add.existing(paddleRight, true);
+    this.physics.add.collider(paddleLeft, ball);
+    this.physics.add.collider(paddleRight, ball);
   }
 }
