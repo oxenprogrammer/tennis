3c7ffd390fc448248d0be8af3f4bc3126c595e43
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-11 19:00:37 +0300

    Feat(game): add reset ball

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -9,25 +9,26 @@ export default class Game extends Phaser.Scene {
 
   leftKey;
 
+  ball;
+
   preload() {}
 
   create() {
-    const ball = this.add.circle(400, 250, 10, 0xffffff, 1);
-    this.physics.add.existing(ball);
-    ball.body.setCollideWorldBounds(true, 1, 1);
+    this.physics.world.setBounds(-100, 0, 1000, 500);
+    this.ball = this.add.circle(400, 250, 10, 0xffffff, 1);
+    this.physics.add.existing(this.ball);
+    this.ball.body.setCollideWorldBounds(true, 1, 1);
 
-    const angle = Phaser.Math.Between(10, 360);
-    const vector = this.physics.velocityFromAngle(angle, 300);
-    ball.body.setVelocity(vector.x, vector.y);
-    ball.body.setBounce(1, 1);
+    this.resetBall();
+    this.ball.body.setBounce(1, 1);
 
     this.paddleLeft = this.add.rectangle(30, 250, 25, 100, 0x0000ff, 1);
     this.paddleRight = this.add.rectangle(770, 250, 25, 100, 0xff0000, 1);
 
     this.physics.add.existing(this.paddleLeft, true);
     this.physics.add.existing(this.paddleRight, true);
-    this.physics.add.collider(this.paddleLeft, ball);
-    this.physics.add.collider(this.paddleRight, ball);
+    this.physics.add.collider(this.paddleLeft, this.ball);
+    this.physics.add.collider(this.paddleRight, this.ball);
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.leftKey = this.input.keyboard;
@@ -58,5 +59,20 @@ export default class Game extends Phaser.Scene {
       this.paddleLeft.y += 10;
       leftBody.updateFromGameObject();
     }
+
+    if (this.ball.x < -30) {
+      this.resetBall();
+    }
+
+    if (this.ball.x > 830) {
+      this.resetBall();
+    }
+  }
+
+  resetBall() {
+    this.ball.setPosition(400, 250);
+    const angle = Phaser.Math.Between(10, 360);
+    const vector = this.physics.velocityFromAngle(angle, 300);
+    this.ball.body.setVelocity(vector.x, vector.y);
   }
 }
