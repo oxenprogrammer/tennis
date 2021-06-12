cbdad1022a4ed9c8c610f2038d4aeb53870658da
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-12 11:25:24 +0300

    Feat(game): add Press Start 2P font

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import Phaser from 'phaser';
+import WebFontFile from '../../webFontFile';
 
 export default class Game extends Phaser.Scene {
   init() {
@@ -25,6 +26,11 @@ export default class Game extends Phaser.Scene {
 
   rightScoreLabel;
 
+  preload() {
+    const fonts = new WebFontFile(this.load, 'Press Start 2P');
+    this.load.addFile(fonts);
+  }
+
   create() {
     this.physics.world.setBounds(-100, 0, 1000, 500);
     this.ball = this.add.circle(400, 250, 10, 0xffffff, 1);
@@ -45,13 +51,15 @@ export default class Game extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
     this.leftKey = this.input.keyboard;
 
-    this.leftScoreLabel = this.add.text(200, 24, '0');
+    this.leftScoreLabel = this.add.text(100, 24, '0');
     this.leftScoreLabel.setColor('blue');
-    this.leftScoreLabel.setFontSize(36);
+    this.leftScoreLabel.setFontSize(24);
+    this.leftScoreLabel.setFontFamily('"Press Start 2P"');
 
-    this.rightScoreLabel = this.add.text(600, 24, '0');
+    this.rightScoreLabel = this.add.text(500, 24, '0');
     this.rightScoreLabel.setColor('red');
-    this.rightScoreLabel.setFontSize(36);
+    this.rightScoreLabel.setFontSize(24);
+    this.rightScoreLabel.setFontFamily('"Press Start 2P"');
   }
 
   update() {
